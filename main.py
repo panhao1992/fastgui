159b5143aa729297a366325cf483e6b84280da6f
panhao <panhao.1992@bytedance.com>
2025-01-13 17:22:46 +0800

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding;utf-8 -*-
+"""
+File name : main.PY
+Create file time: 2023/4/11 19:02
+File Create By Author : Admin
+"""
+from pico_tools.tools_index import *
+
+if __name__ == '__main__':
+    picovr = Tk(className=' PICO嵌入式开发本地工具平台')
+    Index(picovr)
+    picovr.mainloop()
